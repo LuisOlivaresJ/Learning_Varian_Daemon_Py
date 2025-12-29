5b007b9c4ac506c2cb18e9c4738d66958c361956
Luis Olivares <alfonso.cucei.udg@gmail.com>
2025-12-29 11:43:57 -0700

    C-MOVE functionality

--- scripts/03_get_image.py
+++ scripts/03_get_image.py
@@ -1,12 +1,15 @@
 import os
+from pathlib import Path
 
-from pynetdicom import AE, debug_logger, Association
-from pynetdicom.sop_class import SOPClass
+from pynetdicom import AE, debug_logger, Association, build_role, evt, build_context
+
 from pydicom.dataset import Dataset
 
 from dotenv import load_dotenv
 
+
 #debug_logger()
+
 
 # Load environment variables (AE Title, IP, Port)
 load_dotenv()  # This loads the variables from .env file
@@ -14,15 +17,18 @@ VarianDB_PORT = os.getenv("VARIAN_DB_PORT")
 VarianDB_AET = os.getenv("VARIAN_DB_AET")
 VarianDB_IP = os.getenv("VARIAN_DB_IP")
 
+PyNETDICOM_IP = os.getenv("PYNETDICOM_IP")
+PyNETDICOM_AET = os.getenv("PYNETDICOM_AET")
+PyNETDICOM_PORT = os.getenv("PYNETDICOM_PORT")
+
 
 # SOP Classes used in Presentation Context
 ## Study Root Query/Retrieve Find
 STUDY_ROOT_QR_FIND = "1.2.840.10008.5.1.4.1.2.2.1"
+## Study Root Query/Retrieve Move
+STUDY_ROOT_QR_MOVE = "1.2.840.10008.5.1.4.1.2.2.2"
 ## SOP Class UID
 RT_IMAGE_CLASS_UID = "1.2.840.10008.5.1.4.1.1.481.1"
-
-
-MY_AET = "FM_SCU"  # My AE Title
 
 
 # Constant query's attributes (clinic specific)
@@ -33,7 +39,8 @@ STUDY_ID = "RapidArc QA Test"  # This corresponds to the Course Name in Eclipse
 def get_series_UIDs(
         patient_id: str,
         study_id: str,
-        assoc: Association
+        assoc: Association,
+        date: str = "19910318-",  # Query dates starting on 1991 March 18 TODO
         ) -> set[str]:
     """
     Helper function to get all Series Instance UIDs for a given patient and study.
@@ -47,6 +54,11 @@ def get_series_UIDs(
     ds.QueryRetrieveLevel = "SERIES"
     ds.PatientID = patient_id
     ds.StudyID = study_id
+
+    # We are interested only in RT Images
+    ds.Modality = "RTIMAGE"
+    #ds.ContentDate = date TODO It is not suported in SERIES level
+
     ds.SeriesInstanceUID = ""
 
     # Send the C-FIND request
@@ -78,7 +90,7 @@ def get_image_UIDs(
             study_id (str): The study ID.
             serie_UID (str): The series instance UID.
             date (str): The date to query. Should be a valid matching format according to DICOM standards 
-                (e.g., "20250102-" for dates starting on 2025 January 02). See DICOM Standard PS3.4 Section C.2.2.2 Attribute Matching for details.
+                (e.g., "20250102-" for dates starting on 2025 January 02). See DICOM Standard PS3.4 Section Attribute Matching for details.
 
         Returns:
             set[str]: A set of Image SOP Instance UIDs.
@@ -112,31 +124,85 @@ def get_image_UIDs(
         return image_UIDs
 
 
+# Implement the handler for evt.EVT_C_STORE
+def handle_store(event):
+    """Handle a C-STORE request event."""
+    ds = event.dataset
+    ds.file_meta = event.file_meta
+
+    # Save the dataset using the SOP Instance UID as the filename
+    path_to_save = Path("/home/luis/FM/testing_DICOM_C-MOVE") / ds.SOPInstanceUID
+    ds.save_as(path_to_save, enforce_file_format=True)
+
+    # Return a 'Success' status
+    return 0x0000
+
+handlers = [(evt.EVT_C_STORE, handle_store)]
+
+
 def main():
     print("Hello from Learning-Varian-Daemon-with-Python!")
 
     # Create an Application Entity
-    ae = AE(MY_AET)  # User AE Title
-    ae.add_requested_context(STUDY_ROOT_QR_FIND)
+    ae = AE(PyNETDICOM_AET)  # AE Title
+    ae.add_requested_context(STUDY_ROOT_QR_FIND)  # C-FIND
+    ae.add_requested_context(STUDY_ROOT_QR_MOVE)  # C-MOVE
+    ae.add_requested_context(RT_IMAGE_CLASS_UID)  # RT Image SOP Class
+
+    # Define the presentation contexts for the Storage SCP
+    contexts = [build_context(RT_IMAGE_CLASS_UID)]
 
     # Create an association with Varian DB
     assoc = ae.associate(
         addr = VarianDB_IP,
         port = int(VarianDB_PORT),
-        ae_title = VarianDB_AET
+        ae_title = VarianDB_AET,
+    )
+
+    # Start our Storage SCP in non-blocking mode
+    scp = ae.start_server(
+        address=(PyNETDICOM_IP, int(PyNETDICOM_PORT)),
+        block=False,
+        evt_handlers=handlers,
+        contexts=contexts,
     )
 
     if assoc.is_established:
         print("Association established!")
 
-        image_UIDs = set()  # To hold all image unique Image UIDs
+        #image_UIDs = set()  # To hold all image unique Image UIDs
         try:
-            serie_uids = get_series_UIDs(PATIENT_ID, STUDY_ID, assoc)
+            # FIND
+            serie_uids = get_series_UIDs(PATIENT_ID, STUDY_ID, assoc, date="20251101-")
 
-            for serie_uid in serie_uids:
+            """ for serie_uid in serie_uids:
                 image_UIDs.update(get_image_UIDs(PATIENT_ID, STUDY_ID, serie_uid, "20251101-", assoc))
+                break  # Uncomment to process only first series
                 
-            print(f"Number of image UIDs: {len(image_UIDs)}")
+            print(f"Number of image UIDs: {len(image_UIDs)}") """
+
+            # MOVE
+            for serie_uid in serie_uids:
+
+                # Create the query dataset and send the C-MOVE request
+                ds = Dataset()
+                ds.QueryRetrieveLevel = "SERIES"
+                ds.PatientID = PATIENT_ID
+                ds.StudyID = STUDY_ID
+                ds.SeriesInstanceUID = serie_uid
+
+                if assoc.is_established:
+                    print(f"Requesting C-MOVE for SeriesInstanceUID: {serie_uid}")
+                    responses = assoc.send_c_move(
+                        ds,
+                        PyNETDICOM_AET,
+                        STUDY_ROOT_QR_MOVE
+                    )
+                    for (status, identifier) in responses:
+                        if status:
+                            print(f"C-MOVE response status: 0x{status.Status:04x}")
+                        else:
+                            print("Connection timed out, was aborted or received invalid response")
 
         finally:  # Ensure the association is released
             assoc.release()
@@ -144,7 +210,8 @@ def main():
     else:
         print("Association rejected, aborted or never connected")
 
-
+    # Stop the SCP
+    scp.shutdown()
 
 if __name__ == "__main__":
     main()
