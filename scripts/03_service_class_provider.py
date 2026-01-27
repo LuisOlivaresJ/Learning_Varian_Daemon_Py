f81da03e5d5ff75bbe55f2ea0d0fbe9dc2c668fe
Luis Olivares <alfonso.cucei.udg@gmail.com>
2026-01-27 11:31:33 -0700

    SCP running as a service in linux

--- scripts/03_service_class_provider.py
+++ scripts/03_service_class_provider.py
@@ -1,26 +1,31 @@
 """
-This script should be ejecuted as a service in our server.
-
+This script should be ejecuted in a service in our server.
 
 """
 
 import os
 from pathlib import Path
+import signal
+import logging
+import sys
 
 from pynetdicom import AE, build_context, evt
 
 from dotenv import load_dotenv
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("DICOM-SCP")
 
 # Load environment variables (AE Title, IP, Port)
 load_dotenv()  # This loads the variables from .env file
+
 VarianDB_PORT = os.getenv("VARIAN_DB_PORT")
 VarianDB_AET = os.getenv("VARIAN_DB_AET")
 VarianDB_IP = os.getenv("VARIAN_DB_IP")
 
-PyNETDICOM_IP = os.getenv("PYNETDICOM_IP")
-PyNETDICOM_AET = os.getenv("PYNETDICOM_AET")
-PyNETDICOM_PORT = os.getenv("PYNETDICOM_PORT")
+SCP_IP = os.getenv("PyNetDICOM_SCP_IP")
+SCP_PORT = os.getenv("PyNetDICOM_SCP_PORT")
+SCP_AET = os.getenv("PyNetDICOM_SCP_IP_AET")
 
 # SOP Classes used in Presentation Context
 ## Study Root Query/Retrieve Move
@@ -44,29 +49,35 @@ def handle_store(event):
 
 handlers = [(evt.EVT_C_STORE, handle_store)]
 
+# Create an Application Entity
+ae = AE(SCP_AET)  # AE Title
+ae.add_requested_context(STUDY_ROOT_QR_MOVE)  # C-MOVE
+ae.add_requested_context(RT_IMAGE_CLASS_UID)  # RT Image SOP Class
+
+
+# Shutdown the SCP when the service stop (SIGTERM or SIGINT signals from the OS)
+def shutdown_handler(signum, frame):
+    logger.info(f"Shutdown signal received {signum}. Stopping SCP...")
+    ae.shutdown()
+    sys.exit(0)
+
+signal.signal(signal.SIGTERM, shutdown_handler)
+signal.signal(signal.SIGINT, shutdown_handler)
+
+
 
 def main():
-    print("Hello from Learning-Varian-Daemon-with-Python!")
-
-    # Create an Application Entity
-    ae = AE(PyNETDICOM_AET)  # AE Title
-    ae.add_requested_context(STUDY_ROOT_QR_MOVE)  # C-MOVE
-    ae.add_requested_context(RT_IMAGE_CLASS_UID)  # RT Image SOP Class
+    logger.info("Starting DICOM Storage SCP")
 
     # Define the presentation contexts for the Storage SCP
     contexts = [build_context(RT_IMAGE_CLASS_UID)]
 
-    # Create an association with Varian DB
-    assoc = ae.associate(
-        addr = VarianDB_IP,
-        port = int(VarianDB_PORT),
-        ae_title = VarianDB_AET,
-    )
-
     # Start our Storage SCP in non-blocking mode
-    scp = ae.start_server(
-        address=(PyNETDICOM_IP, int(PyNETDICOM_PORT)),
-        block=False,
+    ae.start_server(
+        address=(SCP_IP, int(SCP_PORT)),
         evt_handlers=handlers,
         contexts=contexts,
     )
+
+if __name__ == "__main__":
+    main()
